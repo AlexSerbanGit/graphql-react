219f2105cc9bc274d424c9d02adb6be3a5de1c74
AlexSerbanGit <alexserb2009@yahoo.com>
2020-03-01 17:53:27 +0200

    React js app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// components
+import BookList from './components/BookList';
+
+function App() {
+  return (
+    <div id="main">
+      <h1>Reading list</h1>
+      <BookList/>
+    </div>
+  );
+}
+
+export default App;
